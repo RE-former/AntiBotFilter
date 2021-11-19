fffe56951602cd249387d56b447f11ec4e7c2520
AmazinAmazon <mati.bertrand@icloud.com>
2021-11-19 18:49:53 +0100

    First Commit

--- src/main/java/com/reformer/antibotfilter/AntiBotFilter.java
+++ src/main/java/com/reformer/antibotfilter/AntiBotFilter.java
@@ -13,5 +13,6 @@ public final class AntiBotFilter extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("AntiBotFilter Stopped");
     }
 }
